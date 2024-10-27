0e724457be9dfa9f5b514f6ba98435e1ccc051a8
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-10-28 03:05:53 +0530

    feat: Add the crowdfunding schema and creation function

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -2,5 +2,57 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    constructor() {}
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint256 target;
+        uint256 deadline;
+        uint256 amountCollected;
+        string image;
+        address[] donators;
+        uint256[] donations;
+    }
+    mapping(uint256 => Campaign) public campaigns;
+    uint256 public numberOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _deadline,
+        uint256 _target,
+        string memory _image
+    ) public returns (uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+        require(
+            campaign.deadling < block.timestamp,
+            "Deadline must be in the future"
+        );
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.deadline = _deadline;
+        campaign.target = _target;
+        campaign.image = _image;
+        numberOfCampaigns++;
+        return numberOfCampaigns - 1;
+    }
+
+    function donateToCampagin(uint256 _id) public payable {
+        uint256 amount = msg.value;
+        Campaign storage campaign = campaigns[_id];
+        require(
+            campaign.deadline > block.timestamp,
+            "Campaign is no longer accepting donations"
+        );
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);;
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+        if (sent) {
+            campaign.amountCollected += amount;
+        }
+    }
+
+    function getDonators() {}
 }
